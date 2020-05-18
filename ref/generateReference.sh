1397189bc4988c2d1828b40e60e23e6e818f5af1
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-05-18 17:04:25 +0200

    Add script to mirror mybuild schedule web page

---
+++ ref/generateReference.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+wget --mirror --span-hosts \
+    --load-cookies mybuild.cookies \
+    --save-cookies mybuild.cookies \
+    --keep-session-cookies \
+    https://mybuild.microsoft.com/schedule
